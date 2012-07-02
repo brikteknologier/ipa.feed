3be0862a828359577ed2e57ead0c0983d92cfe06
Jon Packer <contact@jonpacker.com>
2012-07-03 00:07:38 +0200

    initial commit

---
+++ ipa.feed.js
@@ -0,0 +1,35 @@
+var FeedParser = require('FeedParser'), parser = new FeedParser();
+var async = require('async');
+var _ = require('underscore');
+var ipaClient = require('ipa.client');
+
+module.exports = function(ipaServer, urls) {
+  var publist = [];
+  var ipa = ipaClient(ipaServer);
+
+  function readFeed(url, callback) {
+    parser.parseUrl(url, function(err, meta, articles) {
+      if (err) {
+        callback(err);
+      }
+      callback(null, _.reject(articles, function(article) {
+        var exists = publist.indexOf(article.guid) !== -1;
+        if (!exists) publist.push(article.guid);
+        return exists;
+      }));
+    });
+  }
+
+  function publishArticle(article, callback) {
+    ipa.log(article.title, callback);
+  }
+
+  return function(callback) {
+    async.map(urls, readFeed, function(err, newArticles) {
+      newArticles = _.sortBy(_.flatten(newArticles, true), function(article) {
+        return article.date.getTime();
+      });
+      async.mapSeries(newArticles, publishArticle, callback || function(){});
+    });
+  }
+}
